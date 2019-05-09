c1760e4cf3d4fb093b29e0dc9610dd9294c012e0
ramenjuniti <ramenjuniti@gmail.com>
2019-05-09 22:39:23 +0900

    :+1: test追加

--- src/background/go/main_test.go
+++ src/background/go/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"syscall/js"
-  "testing"
+	"testing"
 )
 
 func TestCodeFormatSuccess(t *testing.T) {
@@ -20,20 +20,32 @@ func TestCodeFormatSuccess(t *testing.T) {
 			`,
 		),
 	}
+
 	result := codeFormat(this, args).(map[string]interface{})
+
 	if _, ok := result["err"]; ok {
 		t.Fatal("failed test")
 	}
 }
 
-// func TestCodeFormatFailed(t *testing.T) {
-// 	this := js.ValueOf(nil)
-// 	args := []js.Value{js.ValueOf("ramen")}
-// 	result := codeFormat(this, args)
-// 	if err == nil {
-// 		t.Fatal("failed test")
-// 	}
-// 	if result != 0 {
-// 		t.Fatal("failed test")
-// 	}
-// }
+func TestCodeFormatFailed(t *testing.T) {
+	this := js.ValueOf(nil)
+	args := []js.Value{
+		js.ValueOf(
+			`
+			package main
+
+			import "fmt"
+			
+			func main() {
+				fmt.Println("Hello, 世界")
+			`,
+		),
+	}
+
+	result := codeFormat(this, args).(map[string]interface{})
+
+	if _, ok := result["newCode"]; ok {
+		t.Fatal("failed test")
+	}
+}
