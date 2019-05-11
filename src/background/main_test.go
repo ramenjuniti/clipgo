a6a327207bea4fe9796faa65fddd7d80ac9795f1
ramenjuniti <ramenjuniti@gmail.com>
2019-05-11 19:14:50 +0900

    :+1: testのerrorメッセージをより具体的に

--- src/background/main_test.go
+++ src/background/main_test.go
@@ -5,27 +5,27 @@ import (
 	"testing"
 )
 
-func TestCodeFormatSuccess(t *testing.T) {
-	this := js.ValueOf(nil)
-	args := []js.Value{
-		js.ValueOf(
-			`
-			package main
+func TestRegisterCallbackFailed(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err == nil {
+			t.Errorf("got %v, want syscall/js: Value.Call: property codeFormat is not a function, got undefined", err)
+		}
+	}()
 
-			import "fmt"
-			
-			func main() {
-				fmt.Println("Hello, 世界")
-			}
-			`,
-		),
-	}
+	js.Global().Call("codeFormat", "hoge")
+}
 
-	result := codeFormat(this, args).(map[string]interface{})
+func TestRegisterCallbackSuccess(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			t.Errorf("got %v, want nil", err)
+		}
+	}()
 
-	if _, ok := result["err"]; ok {
-		t.Fatal("failed test")
-	}
+	registerCallback()
+	js.Global().Call("codeFormat", "hoge")
 }
 
 func TestCodeFormatFailed(t *testing.T) {
@@ -46,6 +46,29 @@ func TestCodeFormatFailed(t *testing.T) {
 	result := codeFormat(this, args).(map[string]interface{})
 
 	if _, ok := result["newCode"]; ok {
-		t.Fatal("failed test")
+		t.Errorf("got newCode, want err")
+	}
+}
+
+func TestCodeFormatSuccess(t *testing.T) {
+	this := js.ValueOf(nil)
+	args := []js.Value{
+		js.ValueOf(
+			`
+			package main
+
+			import "fmt"
+			
+			func main() {
+				fmt.Println("Hello, 世界")
+			}
+			`,
+		),
+	}
+
+	result := codeFormat(this, args).(map[string]interface{})
+
+	if _, ok := result["err"]; ok {
+		t.Errorf("got err, want newCode")
 	}
 }
