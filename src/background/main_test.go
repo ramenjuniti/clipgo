d26a8e2065fc6a3b2322113d0331e8bba822ddd7
ramenjuniti <ramenjuniti@gmail.com>
2019-05-16 15:39:55 +0900

    :+1: codeFormatをjs.Callした時のテスト追加

--- src/background/main_test.go
+++ src/background/main_test.go
@@ -28,7 +28,6 @@ func TestRegisterCallback(t *testing.T) {
 		registerCallback()
 		js.Global().Call("codeFormat", "hoge")
 	})
-
 }
 
 func TestCodeFormat(t *testing.T) {
@@ -109,6 +108,89 @@ func TestCodeFormat(t *testing.T) {
 				t.Errorf("got err = %v, want err = %v", got["err"], test.err)
 			} else if got["output"] != test.output {
 				t.Errorf("got output = %v, want output = %v", got["output"], test.output)
+			}
+		})
+	}
+}
+
+func TestJsCallCodeFormat(t *testing.T) {
+	registerCallback()
+	tests := []struct {
+		name   string
+		input  string
+		output string
+		err    bool
+	}{
+		{
+			name: "success1",
+			input: "" +
+				`package main` + "\n" +
+				"\n" +
+				`import "fmt"` + "\n" +
+				"\n" +
+				`func main() {` + "\n" +
+				`	fmt.Println("Hello, 世界")` + "\n" +
+				`}` + "\n",
+			output: "" +
+				`package main` + "\n" +
+				"\n" +
+				`import "fmt"` + "\n" +
+				"\n" +
+				`func main() {` + "\n" +
+				`	fmt.Println("Hello, 世界")` + "\n" +
+				`}` + "\n",
+			err: false,
+		},
+		{
+			name: "success2",
+			input: "" +
+				`package main` + "\n" +
+				`import "fmt"` + "\n" +
+				`func main() {` + "\n" +
+				`	fmt.Println("Hello, 世界")` + "\n" +
+				`}` + "\n",
+			output: "" +
+				`package main` + "\n" +
+				"\n" +
+				`import "fmt"` + "\n" +
+				"\n" +
+				`func main() {` + "\n" +
+				`	fmt.Println("Hello, 世界")` + "\n" +
+				`}` + "\n",
+			err: false,
+		},
+		{
+			name: "failure1",
+			input: "" +
+				`package main` + "\n" +
+				"\n" +
+				`import "fmt"` + "\n" +
+				"\n" +
+				`func main() {` + "\n" +
+				`	fmt.Println("Hello, 世界")` + "\n",
+			output: "6:31: expected '}', found 'EOF'",
+			err:    true,
+		},
+		{
+			name: "failure2",
+			input: "" +
+				`package main` + "\n" +
+				"\n" +
+				`import "fmt"` + "\n" +
+				"\n" +
+				`func main() {` + "\n",
+			output: "5:15: expected '}', found 'EOF'",
+			err:    true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := js.Global().Call("codeFormat", test.input)
+			if got.Get("err").Bool() != test.err {
+				t.Errorf("got err = %v, want err = %v", got.Get("err").Bool(), test.err)
+			} else if got.Get("output").String() != test.output {
+				t.Errorf("got output = %v, want output = %v", got.Get("output").String(), test.output)
 			}
 		})
 	}
