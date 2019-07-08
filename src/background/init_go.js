55f7d9726cbf09001f08d97c8e6f4c670f52c802
ramenjuniti <ramenjuniti@gmail.com>
2019-07-08 17:59:54 +0900

    change wasm path

--- src/background/init_go.js
+++ src/background/init_go.js
@@ -1,6 +1,6 @@
 const go = new Go();
 
-WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then(
+WebAssembly.instantiateStreaming(fetch("formatter.wasm"), go.importObject).then(
   result => {
     go.run(result.instance);
   }
